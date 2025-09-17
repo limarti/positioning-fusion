37560e9bd707c56fc1665f7b5d209aa58b1e3f30
Lisandro Martinez <lmartinez@geolitix.com>
2025-09-17 11:39:54 -0700

    No USB warning

--- Frontend/src/components/FileLoggingPanel.vue
+++ Frontend/src/components/FileLoggingPanel.vue
@@ -55,9 +55,16 @@ const getFileStatusText = (isActive) => {
       </div>
       <div>
         <h3 class="font-bold text-slate-800">File Logging</h3>
-        <div class="text-xs" :class="getDriveStatusColor(fileLoggingStatus.driveAvailable)">
-          {{ fileLoggingStatus.driveAvailable ? 'Drive Connected' : 'Drive Disconnected' }}
-        </div>
+      </div>
+    </div>
+
+    <!-- Drive Warning -->
+    <div v-if="!fileLoggingStatus.driveAvailable" class="bg-red-50 border border-red-200 rounded-lg p-3 mb-3">
+      <div class="flex items-center space-x-2">
+        <svg class="w-4 h-4 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+          <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
+        </svg>
+        <span class="text-red-700 text-sm font-medium">No USB drive detected</span>
       </div>
     </div>
 
