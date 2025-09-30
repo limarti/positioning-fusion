f8ca5cb9313bb6f5c43e6b42ac0e7b7a8915644b
Lisandro Martinez <lmartinez@geolitix.com>
2025-09-30 14:06:27 -0700

    static component load

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,4 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import GnssView from '@/views/GnssView.vue';
+import CameraView from '@/views/CameraView.vue';
+import ImuView from '@/views/ImuView.vue';
+import EncoderView from '@/views/EncoderView.vue';
+import WiFiView from '@/views/WiFiView.vue';
+import LoggingView from '@/views/LoggingView.vue';
+import SystemView from '@/views/SystemView.vue';
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -10,37 +17,37 @@ const router = createRouter({
     {
       path: '/gnss',
       name: 'gnss',
-      component: () => import('@/views/GnssView.vue')
+      component: GnssView
     },
     {
       path: '/camera',
       name: 'camera',
-      component: () => import('@/views/CameraView.vue')
+      component: CameraView
     },
     {
       path: '/imu',
       name: 'imu',
-      component: () => import('@/views/ImuView.vue')
+      component: ImuView
     },
     {
       path: '/encoder',
       name: 'encoder',
-      component: () => import('@/views/EncoderView.vue')
+      component: EncoderView
     },
     {
       path: '/wifi',
       name: 'wifi',
-      component: () => import('@/views/WiFiView.vue')
+      component: WiFiView
     },
     {
       path: '/logging',
       name: 'logging',
-      component: () => import('@/views/LoggingView.vue')
+      component: LoggingView
     },
     {
       path: '/system',
       name: 'system',
-      component: () => import('@/views/SystemView.vue')
+      component: SystemView
     }
   ]
 });
